2e4ee51e51c36e943891d3cf328f5ee9f82571c4
Ian Marshall <ipgmarshall@gmail.com>
2016-11-24 16:20:41 +0100

    test

--- helper_scripts/short_names_for_phylip.jl
+++ helper_scripts/short_names_for_phylip.jl
@@ -14,7 +14,7 @@ end
 function make_name_short(long_name, existing_names)
     short_name = long_name[1:3]*join(matchall(r"_{1}.{1}",long_name),"")
     
-    if in(short_name[1:10],map(strip,existing_names))
+    if in(short_name,map(strip,existing_names))
         println("Duplicate name found: "*short_name)
         short_name = add_number_to_name(short_name)
         println("New name: "*short_name)
@@ -27,6 +27,11 @@ function make_name_short(long_name, existing_names)
 
     else
         println("truncating name")
+        if in(short_name[1:9]*string(short_name[end]),existing_names))
+            println("Duplicate name found: "*short_name)
+            short_name = add_number_to_name(short_name)
+            println("New name: "*short_name)
+        end
         return short_name[1:9]*string(short_name[end])
     end
 end
